c0f31698229b356951c92816d37b9da20fce9678
Divyansh Kumar Sahoo <divyanshladi20@gmail.com>
2024-07-02 01:53:29 +0530

    website completed

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Unlock unbeatable interest rates on property maortrages,starting as low as 3.5%.Secure your dream home with the market's most competitive offers today.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Shield your investment from market volatility with our fixed-rate mortrage options.Enjoy peace of mind with stable and predictable payments",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Seize the opportunity with our best-in-market prices on properties.Discover unbeatable deals that make your dream home a reality.",
+  },
+];
+export default data;
